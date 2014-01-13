6dc4c30a195146c098bf799b3327674d22c1cb47
giannif <giannif@gmail.com>
2014-01-13 13:36:18 -0500

    Top Panel metadata and share added.

--- Gruntfile.js
+++ Gruntfile.js
@@ -49,7 +49,7 @@ module.exports = function(grunt) {
             },
             all: {
                 files: {
-                    "compiled-templates/template.js": ["src/ad-display/template.html", "src/controls/template.html"]
+                    "compiled-templates/template.js": ["src/ad-display/template.html", "src/controls/template.html", "src/top-panel/top-panel.html"]
                 }
             }
         },
---
+++ src/top-panel/top-panel.js
@@ -0,0 +1,24 @@
+/* global Backbone, Templates, $, TopPanelModel*/
+/* exported TopPanel */
+var TopPanel = Backbone.View.extend({
+    template: Templates["src/top-panel/top-panel.html"],
+    tagName: "div",
+    className: "mtvn-tp",
+    events: {
+        "click .mtvn-tp-share": "onShare"
+    },
+    initialize: function(options) {
+        this.options = TopPanelModel.validate(options);
+        this.render();
+    },
+    render: function() {
+        this.$el.html($(this.template(this.options)));
+    },
+    onShare: function(event) {
+        this.trigger(TopPanel.Events.SHARE, $(event.target).data("share-id"));
+    }
+}, {
+    Events: {
+        SHARE: "share"
+    }
+});
