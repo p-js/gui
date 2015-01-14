a895f55716a271046e2f41953e992532c85a75c9
giannif <giannif@gmail.com>
2015-01-13 16:14:14 -0800

    Also deploy css files

--- grunt/tasks/aws_s3.js
+++ grunt/tasks/aws_s3.js
@@ -9,13 +9,13 @@ module.exports = function() {
 		release: {
 			expand: true,
 			cwd: 'dist/',
-			src: '*.js',
+			src: ['*.js', '*.css'],
 			dest: 'gui/<%= package.version %>/'
 		},
 		build: {
 			expand: true,
 			cwd: 'dist/',
-			src: '*.js',
+			src: ['*.js', '*.css'],
 			dest: 'gui/archive/<%= package.version %><%= grunt.config("buildNumber") %>/'
 		}
 	};
